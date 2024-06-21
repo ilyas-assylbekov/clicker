86aa7a553c42aca7cd4f0122a9624bbaec730e3c
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2024-06-21 23:00:47 +0500

    Setup basic local support

--- src/app/image-tracker/image-tracker.component.ts
+++ src/app/image-tracker/image-tracker.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './image-tracker.component.scss'
 })
 export class ImageTrackerComponent {
+  items = [
+    { imageUrl: 'assets/pirates.webp', count: 0 },
+    { imageUrl: 'assets/isekai.webp', count: 0 },
+    { imageUrl: 'assets/marines.webp', count: 0 }
+  ];
 
+  incrementCount(imageIndex: number) {
+    this.items[imageIndex].count++;
+  }
 }
