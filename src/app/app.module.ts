1e867ce6a1561bcb3df0a5f8dce42151a5317230
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2024-06-24 20:41:58 +0500

    Add Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageTrackerComponent } from './image-tracker/image-tracker.component';
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDas23TqYo6tt7phhhAdl5VomspQz7Vwok",
+
+  authDomain: "clicker-b32aa.firebaseapp.com",
+
+  projectId: "clicker-b32aa",
+
+  storageBucket: "clicker-b32aa.appspot.com",
+
+  messagingSenderId: "434990014933",
+
+  appId: "1:434990014933:web:32ddf9d9f2200664fd1d5c",
+
+  measurementId: "G-S5ZYPKDS6G"
+
+});
+
+export const db = getFirestore();
+
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/image-tracker/image-tracker.component.ts
+++ src/app/image-tracker/image-tracker.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { collection, addDoc, getDocs, getDoc, setDoc } from "firebase/firestore";
+import { db, firebaseApp } from '../app.module';
 
 @Component({
   selector: 'app-image-tracker',
   templateUrl: './image-tracker.component.html',
   styleUrl: './image-tracker.component.scss'
 })
-export class ImageTrackerComponent {
+export class ImageTrackerComponent implements OnInit {
   items = [
     { imageUrl: 'assets/pirates.webp', count: 0 },
     { imageUrl: 'assets/isekai.webp', count: 0 },
     { imageUrl: 'assets/marines.webp', count: 0 }
   ];
 
+  async ngOnInit() {
+    const querySnapshot = await getDocs(collection(db, "imageTracker"));
+    if (querySnapshot.empty) {
+      console.log("Collection does not exist or is empty");
+      // Optionally, initialize the collection with documents if it should exist
+      this.items.forEach(async (item) => {
+        const docRef = await addDoc(collection(db, "imageTracker"), item);
+        console.log("Document written with ID: ", docRef.id);
+      });
+    } else {
+      console.log("Collection exists and has documents");
+      // Additional logic here if needed
+      querySnapshot.forEach((doc) => {
+        this.items.forEach((item) => {
+          if (doc.data()['imageUrl'] === item.imageUrl) {
+            item.count = doc.data()['count'];
+          }
+        })
+      });
+    }
+  }
+
   incrementCount(imageIndex: number) {
     this.items[imageIndex].count++;
+    this.items.forEach(async (item) => {
+      const querySnapshot = await getDocs(collection(db, "imageTracker"));
+      querySnapshot.forEach(async (doc) => {
+        if (doc.data()['imageUrl'] === item.imageUrl) {
+          await setDoc(doc.ref, item);
+        }
+      })
+    })
   }
 }
