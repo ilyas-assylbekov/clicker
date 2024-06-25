96d88d1532c8cb944382acf594123d88c0d8f11d
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2024-06-25 22:06:14 +0500

    Save me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { ImageTrackerComponent } from './image-tracker/image-tracker.component';
 import { environment } from './../environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -15,11 +14,10 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    provideFirebaseApp(() => initializeApp( environment.firebase ) ),
   ],
   providers: [
     provideClientHydration(),
-    provideFirebaseApp(() => initializeApp({"projectId":"clicker-b32aa","appId":"1:434990014933:web:32ddf9d9f2200664fd1d5c","databaseURL":"https://clicker-b32aa-default-rtdb.firebaseio.com","storageBucket":"clicker-b32aa.appspot.com","apiKey":"AIzaSyDas23TqYo6tt7phhhAdl5VomspQz7Vwok","authDomain":"clicker-b32aa.firebaseapp.com","messagingSenderId":"434990014933","measurementId":"G-S5ZYPKDS6G"})),
-    provideFirestore(() => getFirestore()),
   ],
   bootstrap: [AppComponent]
 })
