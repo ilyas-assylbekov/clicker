3ca71b533eb50c622a0b737081f400e39c3c59d5
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2024-06-25 20:51:10 +0500

    Fix Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageTrackerComponent } from './image-tracker/image-tracker.component';
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDas23TqYo6tt7phhhAdl5VomspQz7Vwok",
-
-  authDomain: "clicker-b32aa.firebaseapp.com",
-
-  projectId: "clicker-b32aa",
-
-  storageBucket: "clicker-b32aa.appspot.com",
-
-  messagingSenderId: "434990014933",
-
-  appId: "1:434990014933:web:32ddf9d9f2200664fd1d5c",
-
-  measurementId: "G-S5ZYPKDS6G"
-
-});
-
-export const db = getFirestore();
-
+import { environment } from './../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 @NgModule({
   declarations: [
@@ -35,7 +13,8 @@ export const db = getFirestore();
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp( () => initializeApp( environment.firebase ) )
   ],
   providers: [
     provideClientHydration()
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDas23TqYo6tt7phhhAdl5VomspQz7Vwok",
+        authDomain: "clicker-b32aa.firebaseapp.com",
+        projectId: "clicker-b32aa",
+        storageBucket: "clicker-b32aa.appspot.com",
+        messagingSenderId: "434990014933",
+        appId: "1:434990014933:web:32ddf9d9f2200664fd1d5c",
+        measurementId: "G-S5ZYPKDS6G"
+    }
+};
